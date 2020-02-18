449961479beaf1d3d1f67a45b9d7d30ed5eb5098
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-02-18 16:05:03 +0530

    Added api students/me and documentation

--- backend/strapi/medha/api/student/controllers/student.js
+++ backend/strapi/medha/api/student/controllers/student.js
@@ -1,8 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const bookshelf = require("../../../config/config.js");
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve authenticated student.
+   * @return {Object}
+   */
+  async me(ctx) {
+    const user = ctx.state.user;
+    let data;
+    await bookshelf
+      .model("student")
+      .where({ user: user.id })
+      .fetch({
+        withRelated: [
+          "user.state",
+          "user.zone",
+          "user.rpc",
+          "user.college",
+          "stream",
+          "educations"
+        ]
+      })
+      .then(u => {
+        data = sanitizeEntity(u, {
+          model: strapi.query("student").model
+        });
+      });
+
+    ctx.send(data);
+  }
+};
