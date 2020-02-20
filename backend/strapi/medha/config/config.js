86a92aeeb738ddb7ba89ac41b92ad576010d9da4
shreesh.nirgudkar@gmail.com <shreesh.nirgudkar@gmail.com>
2020-02-20 13:00:29 +0530

    Updated bookshelf config

--- backend/strapi/medha/config/config.js
+++ backend/strapi/medha/config/config.js
@@ -15,23 +15,55 @@ const bookshelf = require("bookshelf")(knex);
  * Registering models for bookshelf
  */
 bookshelf.model("state", {
+  requireFetch: false,
   tableName: "states"
 });
 
 bookshelf.model("zone", {
+  requireFetch: false,
   tableName: "zones"
 });
 
 bookshelf.model("rpc", {
-  tableName: "rpcs"
-});
-
-bookshelf.model("college", {
-  tableName: "colleges"
+  requireFetch: false,
+  tableName: "rpcs",
+  zone() {
+    return this.belongsTo("zone", "zone", "id");
+  },
+  colleges() {
+    return this.hasMany("college", "rpc", "id");
+  }
 });
 
 bookshelf.model("stream", {
   tableName: "streams"
+});
+
+bookshelf.model("collegeStreams", {
+  tableName: "colleges__streams",
+  college() {
+    return this.belongsTo("college", "college_id", "id");
+  },
+  stream() {
+    return this.belongsTo("stream", "stream_id", "id");
+  }
+});
+
+bookshelf.model("college", {
+  tableName: "colleges",
+  streams() {
+    return this.belongsToMany("stream").through(
+      "collegeStreams",
+      "college_id",
+      "stream_id"
+    );
+  },
+  principal() {
+    return this.belongsTo("user", "principal", "id");
+  },
+  admins() {
+    return this.hasMany("user", "college", "id");
+  }
 });
 
 bookshelf.model("education", {
